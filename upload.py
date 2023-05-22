5f55b6b8cdb3fd45df57da9f924d3cdff7527c7d
Htain Lin Shwe <saturngod@gmail.com>
2023-05-22 14:37:41 -0500

    disable noise

--- upload.py
+++ upload.py
@@ -1,5 +1,7 @@
 from pydub import AudioSegment
 import os
+from scipy.io import wavfile
+import noisereduce as nr
 
 def convert_to_wav(input_file):
     # Load the input audio file
@@ -7,9 +9,15 @@ def convert_to_wav(input_file):
 
     # Create the output file path with the .wav extension
     output_file = os.path.splitext(input_file)[0] + '.wav'
+    clean_output_file = os.path.splitext(input_file)[0] + '_clean.wav'
 
     # Convert to WAV format
     audio.export(output_file, format='wav')
+
+    #rate, data = wavfile.read(output_file)
+    # perform noise reduction
+    #reduced_noise = nr.reduce_noise(y=data, sr=rate)
+    #wavfile.write(clean_output_file, rate, reduced_noise)
 
     return output_file
 
